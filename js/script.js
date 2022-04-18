f9d1bf13ec91919d1758d26cde4388897012c1f2
Velmoren <velmoren@yahoo.com>
2022-04-18 22:08:14 +0300

    change theme variants

--- js/script.js
+++ js/script.js
@@ -15,4 +15,53 @@ const changeTheme = () => {
   })
 }
 
+const range = () => {
+  const lowerSlider = document.getElementById('lower');
+  const upperSlider = document.getElementById('upper');
+  const lowerValue = document.getElementById('lowerValue')
+  const upperValue = document.getElementById('upperValue')
+
+  const lowerVal = parseInt(lowerSlider.value);
+  const upperVal = parseInt(upperSlider.value);
+
+
+  const updateValue = (lowerVal, upperVal) => {
+    lowerValue.textContent = lowerVal
+    upperValue.textContent = upperVal
+  }
+
+  upperSlider.addEventListener('input', () => {
+    const lowerVal = parseInt(lowerSlider.value);
+    const upperVal = parseInt(upperSlider.value);
+
+    if (upperVal < lowerVal + 4) {
+      lowerSlider.value = upperVal - 4;
+
+      if (lowerVal == lowerSlider.min) {
+        upperSlider.value = 4;
+      }
+    }
+
+    updateValue(lowerVal, upperVal)
+  });
+
+  lowerSlider.addEventListener('input', () => {
+    const lowerVal = parseInt(lowerSlider.value);
+    const upperVal = parseInt(upperSlider.value);
+
+    if (lowerVal > upperVal - 4) {
+      upperSlider.value = lowerVal + 4;
+
+      if (upperVal == upperSlider.max) {
+        lowerSlider.value = parseInt(upperSlider.max) - 4;
+      }
+    }
+
+    updateValue(lowerVal, upperVal)
+  })
+
+  updateValue(lowerVal, upperVal)
+}
+
 changeTheme()
+range()
